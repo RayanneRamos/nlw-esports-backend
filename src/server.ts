4486f681122b54e5c6e126050fb222a6e07f3fa7
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-09-14 21:57:51 -0300

    Criando a rota GET de ads/id/discord da aplicação

--- src/server.ts
+++ src/server.ts
@@ -78,6 +78,21 @@ app.get('/games/:id/ads', async (request, response) => {
  }));
 });
 
+app.get('/ads/:id/discord', async (request, response) => {
+  const adId = request.params.id;
 
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord: true,
+    },
+    where: {
+      id: adId,
+    }
+  });
+
+  return response.json({
+    discord: ad.discord,
+  });
+});
 
 app.listen(3333);
