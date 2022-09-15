b501f71bbb49cbe6a32c9c6d0cbffc6f8ef5d6d6
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-09-14 21:55:43 -0300

    Criando a rota de games da aplicação

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,20 @@ const prisma = new PrismaClient({
   log: [ 'query' ]
 });
 
+app.get('/games', async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        }
+      }
+    }
+  });
+
+  return response.json(games);
+});
+
 
 
 app.listen(3333);
